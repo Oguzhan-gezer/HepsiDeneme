4e15810276e6688f360b9cbc41708405dfdaf84a
Oguzhan-gezer <ogzhngzr17@gmail.com>
2025-07-08 09:31:44 +0300

    Configurationlar oluşturuldu

---
+++ Infrastructure/YoutubeApi.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bogus;
+using YoutubeApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace YoutubeApi.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            Category category1 = new()
+            {
+                Id = 1,
+                ParentId = 0,
+                Name = "Electronics",
+                Priorty = 1,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now
+            };
+            Category category2 = new()
+            {
+                Id = 2,
+                ParentId = 0,
+                Name = "Moda",
+                Priorty = 2,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now
+            };
+            Category parent1 = new()
+            {
+                Id = 3,
+                ParentId = 1,
+                Name = "Bilgisayar",
+                Priorty = 1,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now
+            };
+            Category parent2 = new()
+            {
+                Id = 4,
+                ParentId = 2,
+                Name = "Kadın",
+                Priorty = 1,
+                IsDeleted = false,
+                CreatedDate = DateTime.Now
+            };
+            builder.HasData(category1, category2, parent1, parent2);
+        }
+    }
+}
